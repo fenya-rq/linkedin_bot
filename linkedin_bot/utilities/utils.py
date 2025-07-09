0b3d6b16b0e850f453e8fbe59ce4060829ac986c
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-09 23:14:04 +0700

    feat(utilities:utils): added new tools

--- linkedin_bot/utilities/utils.py
+++ linkedin_bot/utilities/utils.py
@@ -1,3 +1,5 @@
+import json
+import re
 from logging import Logger
 
 
@@ -10,3 +12,19 @@ def log_writer(logger: Logger, lvl: int, log: str) -> None:
     :param log: Log message
     """
     logger.log(lvl, log)
+
+
+def clean_json(string: str, load: bool = False):
+    json_data = re.search(r'```(?:json)?\s*(.*?)```', string, re.DOTALL | re.IGNORECASE)
+    if not json_data:
+        raise Exception('json is not found')
+
+    if load:
+        return json.loads(json_data.group(1))
+
+    return json_data.group(1)
+
+
+def dict_to_string(data: dict):
+    post_to_add = data.get('post')
+    return '\n'.join(f'{k}: {v}' for k, v in post_to_add.items())
