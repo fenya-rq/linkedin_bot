ad7f80f413dadd7f5e73d19eee06137eff2916b8
Yaroslav Panasenko <fenyochek@gmail.com>
2025-07-23 04:12:46 +0700

    refactor: remove redundant modules (langgraph)

--- linkedin_bot/ai/__init__.py
+++
@@ -1,3 +0,0 @@
-from .graph import start_graph
-
-__all__ = ['start_graph']
--- linkedin_bot/ai/configs.py
+++
@@ -1,3 +0,0 @@
-from langchain_core.runnables.config import RunnableConfig
-
-thread_config = RunnableConfig(configurable={'thread_id': '1'})
